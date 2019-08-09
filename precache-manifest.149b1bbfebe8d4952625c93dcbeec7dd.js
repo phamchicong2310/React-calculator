70d45f94bf48a3a577b36b74fb0e059d9bf5ac75
ÔngCông <phamchicong2310@gmail.com>
2019-08-09 09:20:21 +0700

    Updates

---
+++ precache-manifest.149b1bbfebe8d4952625c93dcbeec7dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cecac14591046936c0353b27f70d008e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0952b61624e75741bca1",
+    "url": "/static/css/main.6dc12ec6.chunk.css"
+  },
+  {
+    "revision": "01e4e99017db764479e6",
+    "url": "/static/js/2.60928c47.chunk.js"
+  },
+  {
+    "revision": "0952b61624e75741bca1",
+    "url": "/static/js/main.f56824dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
